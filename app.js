1e0b43608bf998d688e20e0c927c6e6fb8d2973a
kent10ou <kent10ou@gmail.com>
2015-08-04 10:53:17 -0700

    stock lookup works! data aligns with headers

--- app.js
+++ app.js
@@ -1,30 +1,18 @@
-angular.module('StockLookup', [
-	//'StockLookup.services'
-	])
+angular.module('StockLookup', [])
 
 .controller('LookupCtrl', ['$scope', '$http', function ($scope, $http) {
-  $scope.lookup = function () {
-  	var obj = {};
-  	var getQuotes = function () {
-  		console.log('WTF')
-    	$http.get("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22AAPL%22)%0A%09%09&format=json&diagnostics=true&env=http%3A%2F%2Fdatatables.org%2Falltables.env")
-    		.success(function(data) {
-    			console.log('data : ', data)
-      		return obj = data
-    		})
-    		.error(function(err) {
-    			console.log('ERROR: ', err);
-    		})
-  	}
-
-	getQuotes();
-    // console.log(GetQuotesFac.getQuotes())
-    //   .then(function () {
-    //   console.log('hello')
-    // }) 
-    // .catch(function (error) {
-    //   console.log(error);
-    // })
-  }
+	$scope.stockQuotes = {};
+	$scope.stockLookup = function () {
+		$scope.stockQuotes = {};
+		$http.get("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22" + $scope.search.toUpperCase() + "%22)%0A%09%09&format=json&diagnostics=true&env=http%3A%2F%2Fdatatables.org%2Falltables.env")
+		.success(function(data) {
+			console.log('data : ', data.query.results)
+			return $scope.stockQuotes = data.query.results
+		})
+		.error(function(err) {
+			console.log('ERROR: ', err);
+		})
+	}
+	console.log($scope.stockQuotes)
 }])
 
