76e3cc3a7b1823953f1780ff3c5ce2e00f2b887f
kent10ou <kent10ou@gmail.com>
2015-08-03 20:23:32 -0700

    still stuck on grabbing data

--- app.js
+++ app.js
@@ -1,18 +1,58 @@
 angular.module('StockLookup', [])
 
-.controller('LookupCtrl', function ($scope) {
-  $scope.link = {};
-  $scope.addLink = function () {
-    $scope.loading = true;
-    Links.addLink($scope.link)
+.factory('GetQuotesFactory', function ($http) {
+  var obj = {content:null};
+  var getQuotes = function () {
+    $http.get(
+      "./yql.json")
+      //"https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22AAPL%22)%0A%09%09&format=json&diagnostics=true&env=http%3A%2F%2Fdatatables.org%2Falltables.env")
+    .success(function(data) {
+       return obj.content = data
+    })
+  }
+})
+
+.controller('LookupCtrl', function ($scope, GetQuotesFactory) {
+  $scope.quotes = {};
+  $scope.lookup = function () {
+    GetQuotesFactory.getQuotes()
       .then(function () {
-        $scope.loading = false;
-        $location.path('/');
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  };
-});
+      console.log('hello')
+    }) 
+    .catch(function (error) {
+      console.log(error);
+    })
+  }
+})
+
+    
+
+
+    // .then(function(resp) {
+    //     console.log('RESP DATA: ', resp)
+    //     return resp.data;
+    //   })
+
+
+// var app = angular.module('myApp', []);
+// app.controller('customersCtrl', function($scope, $http) {
+//     $http.get("http://www.w3schools.com/angular/customers.php")
+//     .success(function(response) {$scope.names = response.records;});
+// });
+
+  // angular.module('submitExample', [])
+  //   .controller('ExampleController', ['$scope', function($scope) {
+  //     $scope.list = [];
+  //     $scope.text = 'hello';
+  //     $scope.submit = function() {
+  //       if ($scope.text) {
+  //         $scope.list.push(this.text);
+  //         $scope.text = '';
+  //       }
+  //     };
+  //   }]);
+
+
+
 
 
