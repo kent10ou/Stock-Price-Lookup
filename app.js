6738da781984825e4d03ab9f8c8e679fd073466a
kent10ou <kent10ou@gmail.com>
2015-08-04 15:37:39 -0700

    added picture, able to click to remove quote, working on adding ng-class

--- app.js
+++ app.js
@@ -1,18 +1,38 @@
 angular.module('StockLookup', [])
 
 .controller('LookupCtrl', ['$scope', '$http', function ($scope, $http) {
-	$scope.stockQuotes = {};
+	$scope.stockQuotes = [];
 	$scope.stockLookup = function () {
-		$scope.stockQuotes = {};
 		$http.get("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22" + $scope.search.toUpperCase() + "%22)%0A%09%09&format=json&diagnostics=true&env=http%3A%2F%2Fdatatables.org%2Falltables.env")
 		.success(function(data) {
-			console.log('data : ', data.query.results)
-			return $scope.stockQuotes = data.query.results
+			console.log('data : ', data)
+			return $scope.stockQuotes.push(data.query.results.quote);
 		})
 		.error(function(err) {
 			console.log('ERROR: ', err);
 		})
+		$scope.search = '';
+		
 	}
-	console.log($scope.stockQuotes)
+
+	// function removes quotes from page
+	$scope.remove = function (index) {
+		console.log("index : ", index)
+		$scope.stockQuotes.splice(index, 1)
+	};
+
+	// change the entire row to red or green depending on day change
+	// $scope.highlight= function () {
+	// 	for (var i = 0; i < $scope.stockQuotes.length; i++) {
+	// 		console.log('change: ',$scope.stockQuotes[i].Change)
+	// 		if ($scope.stockQuotes[i].Change < 0) {
+	// 			$('.rows').css('background-color: red')
+	// 		} 
+	// 	}
+	// }
+
+
 }])
+
+
 
